a0c60c8a8a3f7e4f0b9cb5bd1fc855f4ac8f0967
HoangHieu <dinhhoanghieu321@gmail.com>
2022-10-20 18:31:28 +0700

    Setup project & Create Login screen

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/theme/pallete.dart';
+
+import 'features/auth/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Reddit Clone',
+      theme: Pallete.darkModeAppTheme,
+      home: const LoginScreen(),
+    );
+  }
+}
+
